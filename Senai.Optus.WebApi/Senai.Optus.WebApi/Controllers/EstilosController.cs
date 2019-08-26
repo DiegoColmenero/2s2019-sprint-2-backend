21c86274e007a4081e86de8ad703eaf1c9f6e080
Diego Alvarez Colmenero <46938238804@info.local>
2019-08-26 11:21:45 -0300

    Optus up

---
+++ Senai.Optus.WebApi/Senai.Optus.WebApi/Controllers/EstilosController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Optus.WebApi.Domains;
+using Senai.Optus.WebApi.Repositories;
+
+namespace Senai.Optus.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstilosController : ControllerBase
+    {
+        EstiloRepository estiloRepository = new EstiloRepository();
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(estiloRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Estilos estilo)
+        {
+            try
+            {
+                estiloRepository.Cadastrar(estilo);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Erro" + ex.Message });
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            estiloRepository.Deletar(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        [HttpPut]
+        public IActionResult Atualizar(Estilos estilo)
+        {
+            try
+            {
+                Estilos estiloBuscado = estiloRepository.BuscarPorId(estilo.IdEstilo);
+                if (estiloBuscado == null)
+                    return NotFound();
+
+                estiloRepository.Atualizar(estilo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+            return Ok();
+
+        }
+    }
+}
---
+++ Senai.Optus.WebApi/Senai.Optus.WebApi/Repositories/EstiloRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Senai.Optus.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Repositories
+{
+    public class EstiloRepository
+    {
+        public List<Estilos> Listar()
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+
+                return ctx.Estilos.ToList();
+
+            }
+        }
+    }
+
+    public Estilos BuscarPorId(int id)
+    {
+        using (OptusContext ctx = new OptusContext())
+        {
+
+            return ctx.Estilos.FirstOrDefault(x => x.IdEstilo == id);
+
+        }
+    }
+
+    public void Cadastrar(Estilos estilo)
+    {
+        using(OptusContext ctx = new OptusContext())
+        {
+            ctx.Estilos.Add(estilo);
+            ctx.SaveChanges();
+        }
+    }
+
+    public void Deletar(int id)
+    {
+        using (OptusContext ctx = new OptusContext())
+        {
+            Estilos estiloBuscado = ctx.Estilos.Find(id);
+            ctx.Estilos.Remove(estiloBuscado);
+            ctx.SaveChanges();
+        }
+    }
+
+    public void Atualizar(Estilos estilo)
+    {
+        using (OptusContext ctx = new OptusContext())
+        {
+            Estilos estiloBuscado = ctx.Estilos.FirstOrDefault(x => x.IdEstilo == estilo.IdEstilo);
+            estiloBuscado.Nome = estilo.Nome;
+            ctx.Estilos.Update(estiloBuscado);
+            ctx.SaveChanges();
+        }
+    }
+}
